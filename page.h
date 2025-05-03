6149cbefafce04edc4cd4fca8e14c5be4595c478
yonaMac <yonaimyo@gmail.com>
2025-05-04 01:05:47 +0900

    Refactor: page 구조체 폐기 및 매크로 함수 추가

--- page.h
+++ page.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
-#define PAGE_SIZE 4096 // 4KB
+#define PAGE_SIZE        4096 // 4KB
+#define PAGE_CAN_COMPACT 0x01
+#define PAGE_DIRTY       0x02
+#define PAGE_FULL        0x04
 
 // A page is a node of B-tree
 enum pageType
@@ -15,44 +18,47 @@ enum pageType
 
 typedef struct Page
 {
-	uint32_t id;
 	uint8_t  buf[PAGE_SIZE];
 } Page;
 
+// metadata of a page
 // location: start of allocated space
 typedef struct PageHeader
 {
-	uint16_t free_start;
-	uint16_t free_end;
-	uint8_t  flag;
+	uint32_t id;
+	enum pageType type;
+	uint16_t      free_start;
+	uint16_t      free_end;
+	uint16_t      free_size; // free_end - free_start
+	uint8_t       flags;
 } PageHeader;
 
 typedef struct RecordPointer
 {
-	uint16_t location;
-	uint16_t size;
+	uint16_t location; // start offset of record
+	uint16_t size;     // size of record
 } RecordPointer;
 
 typedef struct RecordPointerList
 {
 	RecordPointer *list; // e.g list[2]
-	uint16_t       size;
+	uint16_t       size; // size of pointer list
 } RecordPointerList;
 
-#define PAGE_HEADER(page) (pageHeader *)page;
+#define PAGE_HEADER(page) ((PageHeader *)(page))
+#define RECORD_POINTER_LIST(page) ((RecordPointerList *)((u_int8_t *)page + sizeof(PageHeader)))
 
 //------------------------------------------------------------------------------
 
 // In-memory page management (volatile operations)
-Page              *page_create(enum pageType type, uint32_t id);
-void               page_add_record(void *page, void *record, uint16_t size);
-void              *page_get_record(void *page, uint16_t idx);
-void               page_remove_record(void *page, uint16_t idx);
-void               page_compact(void *page);
-RecordPointerList *page_record_pointer_list(void *page);
+void *page_create(enum pageType type, uint32_t id);
+void  page_add_record(void *page, void *record, uint16_t size);
+void *page_get_record(void *page, uint16_t idx);
+void  page_remove_record(void *page, uint16_t idx);
+void  page_compact(void *page);
 
 // Persistent disk I/O (non-volatile storage)
 void  save_page(int fd, void *page);
-Page *load_page(int fd, uint32_t id);
+void *load_page(int fd, uint32_t id);
 
 #endif
